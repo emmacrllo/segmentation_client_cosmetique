3534124a5a45c601f9eb9d7f93fe496f4eaf60bc
emmacrllo <163746545+emmacrllo@users.noreply.github.com>
2024-05-25 21:49:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,17 @@ import matplotlib.pyplot as plt
 # Générer des données aléatoires
 data = np.random.randn(1000)
 
+# Créer une figure
+fig, ax = plt.subplots()
+
+# Créer l'histogramme sur la figure
+ax.hist(data, bins=20)
+
 # Afficher un titre
 st.title("Visualisation d'un histogramme")
 
 # Afficher un sous-titre
 st.subheader("Histogramme de données aléatoires")
 
-# Afficher l'histogramme
-plt.hist(data, bins=20)
-st.pyplot()
+# Afficher l'histogramme avec st.pyplot()
+st.pyplot(fig)
